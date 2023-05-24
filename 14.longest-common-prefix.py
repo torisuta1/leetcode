8f455d4867a449f961c41b17b5020f6cd2ead3a1
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-05-25 02:25:29 +0900

    [14]

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -7,7 +7,18 @@
 # @lc code=start
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_str = min(strs)
+        max_str = max(strs)
+        matched_str = ""
+        for str in min_str:
+            if max_str.startswith(matched_str + str):
+                matched_str += str
+            else:
+                break
+        return matched_str 
         
+
+
         
 # @lc code=end
 
