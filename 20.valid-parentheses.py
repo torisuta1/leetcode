6ef7949c6c76877c29b831acf5622719516755da
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-06-06 23:02:03 +0900

    [20]

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        stack = []
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if char == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif char == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif char == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        return not stack
+
 # @lc code=end
 
