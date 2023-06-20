40fd549289961f2dfc2bbb951d494f9f8883e5e4
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-06-21 05:23:22 +0900

    [217]

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            else:
+                s.add(num)
+        return False
+
         
 # @lc code=end
 
