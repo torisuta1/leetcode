c004c9773fe8cc94277bd79139774efc7bc36c10
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-06-24 09:23:17 +0900

    [20]

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -8,21 +8,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        for char in s:
-            if char == '(' or char == '{' or char == '[':
-                stack.append(char)
-            else:
-                if not stack:
-                    return False
-                if char == ')' and stack[-1] == '(':
-                    stack.pop()
-                elif char == '}' and stack[-1] == '{':
-                    stack.pop()
-                elif char == ']' and stack[-1] == '[':
-                    stack.pop()
-                else:
-                    return False
-        return not stack
+        parentheses_dict = {")": "(", "}": "{", "]": "["}
+        for s in s:
+            if not stack: 
+                stack.append(s)
+                continue
+            if s == ")" and stack[-1] == parentheses_dict[s]:
+                stack.pop(-1)
+                continue
+            if s == "}" and stack[-1] == parentheses_dict[s]: 
+                stack.pop(-1)
+                continue
+            if s == "]" and stack[-1] == parentheses_dict[s]: 
+                stack.pop(-1)
+                continue
+            stack.append(s)
+        return True if not stack else False
+                
+                
+
+
+        
 
 # @lc code=end
 
