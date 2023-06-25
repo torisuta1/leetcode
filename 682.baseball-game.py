063f4a2d25f04ef9aa8736e4f85b34776ffc772e
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-06-25 21:32:22 +0900

    [682]

---
+++ 682.baseball-game.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=682 lang=python3
+#
+# [682] Baseball Game
+#
+
+# @lc code=start
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+# @lc code=end
+
