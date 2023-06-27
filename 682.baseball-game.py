346eab39b2480165a49d8f5d84ec6212d9239a1a
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-06-27 10:30:20 +0900

    [682]

--- 682.baseball-game.py
+++ 682.baseball-game.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for char in operations:
+            if char == "+":
+                stack.append(stack[-1] + stack[-2])
+                continue
+            if char == "D":
+                stack.append(stack[-1] * 2)
+                continue
+            if char == "C":
+                stack.pop()
+                continue
+            stack.append(int(char))
+        return sum(stack)
+                
+
         
 # @lc code=end
 
