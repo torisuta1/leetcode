759f63adc06dc00d019ac3bf022cdfc345381cc0
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-07-03 23:20:53 +0900

    [680]

--- 680.valid-palindrome-ii.py
+++ 680.valid-palindrome-ii.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                skipL = s[left + 1:right + 1]
+                skipR = s[left:right]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+            left, right = left +1, right -1
+        return True
         
 # @lc code=end
 
