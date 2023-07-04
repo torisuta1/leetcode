c3e619693a88f31bf4a837ee3e462c26fcf843d3
torisuta1 <kokopi.6713@tiara.ocn.ne.jp>
2023-07-04 23:10:30 +0900

    [1984]

---
+++ 1984.minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1984 lang=python3
+#
+# [1984] Minimum Difference Between Highest and Lowest of K Scores
+#
+
+# @lc code=start
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
